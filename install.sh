cea2e9a338db3c4b8eaf78587018d2de80f8671a
manhokok <132220167+manhokok@users.noreply.github.com>
2024-10-10 10:39:32 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Cập nhật các gói
+apt update && apt upgrade -y
+
+# Cài đặt git
+apt install git -y
+
+# Setup storage
+mkdir -p /data/data/com.termux/files/home/storage
+
+# Cài đặt ffmpeg
+apt install ffmpeg -y
+
+# Clone repository Peridot
+git clone https://github.com/manhokok/ToolSdcard.git
+
+# Di chuyển tất cả file vào thư mục chính của Termux
+mv ToolSdcard/* /data/data/com.termux/files/home/
+
+# Xóa thư mục Peridot đã clone
+rm -rf ToolSdcard
+rm -rf README.md
+rm -rd install.sh
+echo 'alias mp3="sh mp3"' >> ~/.bashrc
+echo 'alias scanmp3="sh scanmp3"' >> ~/.bashrc
+echo 'alias scanmp4="sh scanmp4"' >> ~/.bashrc
+source ~/.bashrc
